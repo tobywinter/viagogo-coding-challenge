fa4428cc3f2c087b1fe04e08f57670a240210145
Toby Winter <toby.hw@live.co.uk>
2017-08-04 14:34:18 +0100

    Order tickets & find cheapest ticket
    Adds
    - tickets_in_ascending_value private method to Event
    - cheapest_ticket method to Event

--- lib/event.rb
+++ lib/event.rb
@@ -2,7 +2,6 @@ class Event
 
   attr_reader :location, :tickets
 
-
   def initialize(seed_generator = SeedGenerator.new)
     @location = seed_generator.location
     @tickets = []
@@ -10,6 +9,16 @@ class Event
 
   def add_ticket(ticket = Ticket.new)
     @tickets << ticket
+  end
+
+  def cheapest_ticket
+    tickets_in_ascending_value[0]
+  end
+  
+  private
+
+  def tickets_in_ascending_value
+    @tickets.sort_by { |ticket| ticket.price  }
   end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -2,6 +2,14 @@
 describe Event do
   subject(:event) { described_class.new }
 
+  let(:ticket_1) { double(:ticket) }
+  let(:ticket_2) { double(:ticket) }
+
+  before do
+    allow(ticket_1).to receive(:price).and_return(1000)
+    allow(ticket_2).to receive(:price).and_return(3000)
+  end
+
   it 'creates a new event' do
     expect(event).to be_truthy
   end
@@ -28,7 +36,10 @@ describe Event do
     expect(event.tickets.count).to be > 1
   end
 
-  it 'knows the price of tickets' do
+  it 'knows the cheapest ticket available' do
+    event.add_ticket(ticket_2)
+    event.add_ticket(ticket_1)
+    expect(event.cheapest_ticket).to eq ticket_1
   end
 
 end
